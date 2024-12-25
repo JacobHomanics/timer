8ce564b46dca7d69e1162b4c22ca1b15bfcb9da2
Jacob Homanics <homanicsjake@gmail.com>
2024-12-24 18:45:58 -0600

    Update TimerImageWithText.cs

--- Assets/Timer/Scripts/UI/TimerImageWithText.cs
+++ Assets/Timer/Scripts/UI/TimerImageWithText.cs
@@ -5,27 +5,58 @@ namespace JacobHomanics.Core.Timer.UI
 {
     public class TimerImageWithText : MonoBehaviour
     {
-        public enum DisplayType
+        public enum ImageDisplayType
         {
             Elapsed,
             TimeLeft
         }
 
-        public DisplayType displayType;
+        public enum TextDisplayType { Duration, ElapsedTime, TimeLeft }
 
+        [Header("References")]
         public Timer timer;
         public Image image;
+        public Text text;
+
+        [Header("Configuration")]
+        public ImageDisplayType imageDisplayType;
+        public TextDisplayType textDisplayType;
+
+        public string textFormat;
 
         void Update()
         {
-            if (displayType == DisplayType.Elapsed)
+            if (imageDisplayType == ImageDisplayType.Elapsed)
             {
                 image.fillAmount = timer.elapsedTime / timer.duration;
             }
 
-            if (displayType == DisplayType.TimeLeft)
+            if (imageDisplayType == ImageDisplayType.TimeLeft)
             {
                 image.fillAmount = timer.GetTimeLeft() / timer.duration;
+            }
+
+            if (text != null)
+            {
+                SetText(text, textDisplayType, textFormat);
+            }
+        }
+
+        void SetText(Text text, TextDisplayType displayType, string format)
+        {
+            if (displayType == TextDisplayType.Duration)
+            {
+                text.text = timer.duration.ToString(format);
+            }
+
+            if (displayType == TextDisplayType.ElapsedTime)
+            {
+                text.text = timer.elapsedTime.ToString(format);
+            }
+
+            if (displayType == TextDisplayType.TimeLeft)
+            {
+                text.text = timer.GetTimeLeft().ToString(format);
             }
         }
     }
