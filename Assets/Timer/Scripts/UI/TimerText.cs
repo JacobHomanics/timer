b69449676c8b68b1ba579c53df6889ef134de0bf
Jacob Homanics <homanicsjake@gmail.com>
2024-12-24 18:38:51 -0600

    added formatting to timertext

--- Assets/Timer/Scripts/UI/TimerText.cs
+++ Assets/Timer/Scripts/UI/TimerText.cs
@@ -9,6 +9,8 @@ namespace JacobHomanics.Core.Timer.UI
 
         public Text text;
 
+        public string format = "F3";
+
         public enum DisplayType { Duration, ElapsedTime, TimeLeft }
 
         public DisplayType displayType;
@@ -17,17 +19,17 @@ namespace JacobHomanics.Core.Timer.UI
         {
             if (displayType == DisplayType.Duration)
             {
-                text.text = timer.duration.ToString();
+                text.text = timer.duration.ToString(format);
             }
 
             if (displayType == DisplayType.ElapsedTime)
             {
-                text.text = timer.elapsedTime.ToString();
+                text.text = timer.elapsedTime.ToString(format);
             }
 
             if (displayType == DisplayType.TimeLeft)
             {
-                text.text = timer.GetTimeLeft().ToString();
+                text.text = timer.GetTimeLeft().ToString(format);
             }
         }
     }
