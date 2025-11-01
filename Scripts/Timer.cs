57a458925cb663f51da19576270342ceb71d3002
Jacob Homanics <32080359+JacobHomanics@users.noreply.github.com>
2025-11-01 16:29:53 -0500

    initial timer script

---
+++ Scripts/Timer.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace JacobHomanics.TimerSystem
+{
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField] private float duration;
+        public float Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+            }
+        }
+
+        [SerializeField] private float elapsedTime;
+        public float ElapsedTime
+        {
+            get
+            {
+                return elapsedTime;
+            }
+            set
+            {
+                elapsedTime = value;
+                if (elapsedTime >= duration)
+                    OnDurationElapsed?.Invoke();
+            }
+        }
+
+        public enum TickType
+        {
+            DeltaTime, UnscaledDeltaTime, SmoothDeltaTime, FixedDeltaTime, FixedUnscaledDeltaTime
+        }
+
+        [Header("Configuration")]
+        public TickType tickType;
+
+        [Header("Events")]
+        public UnityEvent OnTick = new();
+        public UnityEvent OnDurationElapsed = new();
+        // public UnityEvent OnElapsedTimeReset = new();
+        // public UnityEvent OnElapsedTimeChangedByOffset = new();
+        // public UnityEvent OnDurationChangedByOffset = new();
+
+        ////////////////////////////
+        //Monobehaviour
+        ////////////////////////////
+
+        void Update()
+        {
+            if (tickType == TickType.DeltaTime)
+            {
+                Tick(Time.deltaTime);
+            }
+
+            if (tickType == TickType.SmoothDeltaTime)
+            {
+                Tick(Time.smoothDeltaTime);
+            }
+
+            if (tickType == TickType.UnscaledDeltaTime)
+            {
+                Tick(Time.unscaledDeltaTime);
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (tickType == TickType.FixedDeltaTime)
+            {
+                Tick(Time.fixedDeltaTime);
+            }
+
+            if (tickType == TickType.FixedUnscaledDeltaTime)
+            {
+                Tick(Time.fixedUnscaledDeltaTime);
+            }
+        }
+
+        ////////////////////////////
+        // Core
+        ////////////////////////////
+
+        public void Tick(float delta)
+        {
+            elapsedTime += delta;
+            OnTick?.Invoke();
+        }
+
+        private void OnXGreaterThanOrEqualToY()
+        {
+        }
+
+        ////////////////////////////
+        //Helper Methods
+        ////////////////////////////
+
+        public float GetTimeLeft()
+        {
+            return Duration - ElapsedTime;
+        }
+
+        public bool IsDurationReached()
+        {
+            return ElapsedTime >= Duration;
+        }
+    }
+}
