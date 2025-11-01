b1cc9773560c2dacfb08d71849dd35ff36f76b5e
Jacob Homanics <32080359+JacobHomanics@users.noreply.github.com>
2025-11-01 16:12:54 -0500

    changed to upm architecture

--- Assets/DebugHelper.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DebugHelper : MonoBehaviour
-{
-    public void Log(string message)
-    {
-        Debug.Log(message);
-    }
-}
--- Assets/Supercharged Vector2/Scripts/Vector2Source.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace JacobHomanics.Core.SuperchargedVector2
-{
-    public abstract class Vector2Source : MonoBehaviour
-    {
-        public abstract Vector2 GetReference();
-    }
-}
