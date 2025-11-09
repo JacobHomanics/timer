2023432660a3ce448d1d2a97f2c5435a7095d300
Jacob Homanics <32080359+JacobHomanics@users.noreply.github.com>
2025-11-08 20:23:28 -0600

    Update TimerImage.cs

--- Scripts/UI/TimerImage.cs
+++ Scripts/UI/TimerImage.cs
@@ -1,27 +1,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using JacobHomanics.TimerSystem;
 
-namespace JacobHomanics.TimerSystem
+public class TimerImage : MonoBehaviour
 {
-    public class TimerImage : MonoBehaviour
+    [SerializeField] private Timer timer;
+
+    public Timer Timer
     {
-        [SerializeField] private Timer timer;
+        get => timer;
+    }
 
-        public Timer Timer
-        {
-            get => timer;
-        }
+    [SerializeField] private Image image;
 
-        [SerializeField] private Image image;
+    public Image Image
+    {
+        get => image;
+    }
 
-        public Image Image
-        {
-            get => image;
-        }
-
-        [SerializeReference]
-        private List<FeatureToggle> featureToggles = new List<FeatureToggle>
+    [SerializeReference]
+    private List<FeatureToggle> featureToggles = new List<FeatureToggle>
     {
         new TextDisplayFeature2(),
         new ColorGradientFeature(),
@@ -29,37 +28,36 @@ namespace JacobHomanics.TimerSystem
         new FlashingFeature()
     };
 
-        public float CurrentNum => Timer.ElapsedTime;
-        public float MaxNum => Timer.Duration;
+    public float CurrentNum => Timer.ElapsedTime;
+    public float MaxNum => Timer.Duration;
 
-        public bool reverseFill;
+    public bool reverseFill;
 
-        private float previousValue;
+    private float previousValue;
 
-        private void Awake()
-        {
-            previousValue = CurrentNum;
-        }
-
-        void Update()
-        {
-            Debug.Log(reverseFill);
-            if (reverseFill)
-                image.fillAmount = (MaxNum - CurrentNum) / MaxNum;
-            else
-                image.fillAmount = CurrentNum / MaxNum;
-
-
-
-            UIToolkit.Display(UIToolkit.GetFeature<TextDisplayFeature2>(featureToggles), CurrentNum, MaxNum);
-
-            UIToolkit.ColorGradientFeatureCommand(UIToolkit.GetFeature<ColorGradientFeature>(featureToggles), image, CurrentNum, MaxNum);
-
-            UIToolkit.FlashingFeatureCommand(UIToolkit.GetFeature<FlashingFeature>(featureToggles), CurrentNum, MaxNum);
-
-            UIToolkit.HandleValueChange(CurrentNum, UIToolkit.GetFeature<BackgroundFillFeature>(featureToggles), ref previousValue, MaxNum);
-            UIToolkit.UpdateBackgroundFillAnimation(UIToolkit.GetFeature<BackgroundFillFeature>(featureToggles), MaxNum);
-        }
-
+    private void Awake()
+    {
+        previousValue = CurrentNum;
     }
+
+    void Update()
+    {
+        Debug.Log(reverseFill);
+        if (reverseFill)
+            image.fillAmount = (MaxNum - CurrentNum) / MaxNum;
+        else
+            image.fillAmount = CurrentNum / MaxNum;
+
+
+
+        UIToolkit.Display(UIToolkit.GetFeature<TextDisplayFeature2>(featureToggles), CurrentNum, MaxNum);
+
+        UIToolkit.ColorGradientFeatureCommand(UIToolkit.GetFeature<ColorGradientFeature>(featureToggles), image, CurrentNum, MaxNum);
+
+        UIToolkit.FlashingFeatureCommand(UIToolkit.GetFeature<FlashingFeature>(featureToggles), CurrentNum, MaxNum);
+
+        UIToolkit.HandleValueChange(CurrentNum, UIToolkit.GetFeature<BackgroundFillFeature>(featureToggles), ref previousValue, MaxNum);
+        UIToolkit.UpdateBackgroundFillAnimation(UIToolkit.GetFeature<BackgroundFillFeature>(featureToggles), MaxNum);
+    }
+
 }
